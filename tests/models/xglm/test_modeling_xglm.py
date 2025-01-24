11618b4eff25b996fa2556ea7e4b6e04e5ee0f8a
Damiano Amatruda <damiano.amatruda@outlook.com>
2025-01-24 21:05:38 +0100

    Update expected output string in XGLM sample test
    This updates the expected output string of test_xglm_sample for torch
    2.0 to the correct one and removes the one for torch 1.13.1 + cu116
    (transformers moved to torch 2.0 with PR #35358).

--- tests/models/xglm/test_modeling_xglm.py
+++ tests/models/xglm/test_modeling_xglm.py
@@ -425,14 +425,11 @@ class XGLMModelLanguageGenerationTest(unittest.TestCase):
         output_ids = model.generate(input_ids, do_sample=True, num_beams=1)
         output_str = tokenizer.decode(output_ids[0], skip_special_tokens=True)
 
-        EXPECTED_OUTPUT_STRS = [
-            # TODO: remove this once we move to torch 2.0
-            # torch 1.13.1 + cu116
-            "Today is a nice day and the sun is shining. A nice day with warm rainy",
-            # torch 2.0 + cu117
-            "Today is a nice day and the water is still cold. We just stopped off for some fresh",
-        ]
-        self.assertIn(output_str, EXPECTED_OUTPUT_STRS)
+        EXPECTED_OUTPUT_STR = (
+            "Today is a nice day and the water is still cold. We just stopped off for some fresh coffee. This place"
+            " looks like a"
+        )
+        self.assertEqual(output_str, EXPECTED_OUTPUT_STR)
 
     @slow
     def test_xglm_sample_max_time(self):
